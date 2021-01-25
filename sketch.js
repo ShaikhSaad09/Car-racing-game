6596dc8e9d11a9416d4742985bd4885d0080e967
ShaikhSaad09 <72741582+ShaikhSaad09@users.noreply.github.com>
2021-01-25 17:09:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var gameState=0,playerCount;
+var form,game,player;
+var database,ballposition,position;
+
+function setup(){
+ createCanvas(500,500);
+ database = firebase.database();
+ game = new GameObject();
+ game.getState();
+ game.start();
+}
+
+function draw(){
+
+}
+
